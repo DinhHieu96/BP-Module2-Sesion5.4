18b4b2edb208f95632be7de07ad9a5feb1778be6
DinhHieu96 <truongdinhhieu1496@gamil.com>
2019-08-07 01:06:15 +0700

    Resizeable

---
+++ src/com/company/Circle.java
@@ -0,0 +1,45 @@
+package com.company;
+
+public class Circle extends Shape implements Resizeable {
+    public double radius = 1.0;
+
+    public Circle() {
+
+    }
+
+    public Circle(double radius) {
+
+        this.radius = radius;
+    }
+
+    public Circle(String color, boolean fill, double radius) {
+        super(color, fill);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+
+    public double getPerimeter() {
+        return Math.PI * this.radius * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "A com.company.Main.Circle with radius=" + this.radius + ", which is a subclass of " + super.toString();
+    }
+
+    @Override
+    public void resize(double percent) {
+        this.radius += this.radius * percent / 100;
+    }
+}
---
+++ src/com/company/Shape.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Shape {
+    public String color = "green";
+    public boolean fill = true;
+
+    public Shape() {
+    }
+
+    public Shape(String color, boolean fill) {
+        this.color = color;
+        this.fill = fill;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean getFill() {
+        return this.fill;
+    }
+
+    public void setFill(boolean fill) {
+        this.fill = fill;
+    }
+
+    @Override
+    public String toString() {
+        if (this.fill) {
+            return "A com.company.Main.Shape with color of " + this.color + " and filled";
+        } else {
+            return "A com.company.Main.Shape with color of " + this.color + " and not filled";
+        }
+    }
+}
